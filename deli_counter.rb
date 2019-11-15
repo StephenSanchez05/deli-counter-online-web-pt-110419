79afa6dc5d0249644c648fff3f5867c445f03428
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-15 00:09:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+
+def line(number)
+  if number.empty?
+    puts "The line is currently empty."
+  else
+    deli_line = "The line is currently:"
+    number.each_with_index do |customer, index|
+    deli_line << " #{index+1}. #{customer}"
+end
+puts deli_line
+end
+end
+
+def take_a_number(deli, name)
+  deli << "#{name}"
+  deli.each_with_index do |names, index|
+  puts "Welcome, #{names}. You are number #{index+1} in line."
+end
+end
